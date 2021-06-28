9a0b1af90ad2d1d1dc3fcd019d3f03554c165f23
Adrián Martínez <adrimrtnz@gmail.com>
2021-06-28 21:37:17 +0200

    team icon and tab info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BatteriesService } from './services/batteries.service';
 import { BatteryComponent } from './views/battery/battery.component';
 import { TempSensorComponent } from './views/temp-sensor/temp-sensor.component';
 
